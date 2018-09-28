8f4651c233ec1c6fa3fcde5cb4f23ac5ce99f793
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2018-09-28 09:35:19 +0200

    growstage variable

---
+++ example/Phenology_Model/C#/GrowthStage.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: midingoy
+ * Date: 10/09/2018
+ * Time: 15:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+    public  enum GrowthStage
+    {
+        Unknown,
+        ZC_00_Sowing,
+        ZC_10_Emergence,
+        ZC_21_MainShootPlus1Tiller,
+        ZC_22_MainShootPlus2Tiller,
+        ZC_23_MainShootPlus3Tiller,
+        ZC_30_PseudoStemErection,
+        ZC_31_1stNodeDetectable,
+        ZC_32_2ndNodeDetectable,
+        TerminalSpikelet,
+        ZC_37_FlagLeafJustVisible,
+        ZC_39_FlagLeafLiguleJustVisible,
+        ZC_65_Anthesis,
+        ZC_75_EndCellDivision,
+        ZC_85_MidGrainFilling,
+        ZC_91_EndGrainFilling,
+        ZC_92_Maturity,
+        EndVernalisation,
+        FloralInitiation,
+        Heading,
+        BeginningStemExtension
+    }
